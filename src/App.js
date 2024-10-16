f3610c3bcbe815c6c1fa7c726c86a8798c16c14d
mamedbekov <mamedbekovruslan@yandex.ru>
2024-10-16 15:50:35 +0300

    remove old comments

--- src/App.js
+++ src/App.js
@@ -2,19 +2,16 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import TaskDetails from "./components/TaskDetails";
 import TodoList from "./components/TodoList";
-import NotFound from "./components/NotFound"; // Страница ошибки 404
+import NotFound from "./components/NotFound";
 
 const App = () => {
   return (
     <Router>
       <Routes>
-        {/* Маршрут для главной страницы со списком дел */}
         <Route path="/" element={<TodoList />} />
 
-        {/* Маршрут для страницы задачи с параметром ID */}
         <Route path="/task/:id" element={<TaskDetails />} />
 
-        {/* Маршрут для страницы 404 */}
         <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
